2a5aadc4abc6f9951c0cf272cc1d4ca97657f84d
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-08-22 21:35:17 +0300

    commit tree 4ee9f99256628152742d9c1c5832aac704758f65

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex,  temperament, alive) VALUES ('Mr. Chocolate', 3, 'M',  'Choleric', 1);
+INSERT INTO bears (name, age, sex,  temperament, alive) VALUES ('Rowdy', 5, 'F',  'Phlegmatic', 0);
+INSERT INTO bears (name, age, sex,  temperament, alive) VALUES ('Tabitha', 7, 'F',  'Sanguine', 1);
+INSERT INTO bears (name, age, sex,  temperament, alive) VALUES ('Sergeant Brown', 3, 'M',  'Choleric', 1);
+INSERT INTO bears (name, age, sex,  temperament, alive) VALUES ('Melissa', 4, 'F', 'Sanguine', 1);
+INSERT INTO bears (name, age, sex,  temperament, alive) VALUES ('Grinch', 3, 'M', 'Phlegmatic', 1);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ('Wendy', 2, 'F',  'Melancholic', 0);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES (NULL, 1, 'M',  'Phlegmatic', 1);
